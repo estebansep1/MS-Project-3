39003c355bcc106669dcdd7b5ffd6ec525f7f37c
Esteban Sepulveda <tebansd@icloud.com>
2024-01-24 20:18:56 -0800

    implemented mongodb connection to backend

--- Server/app.py
+++ Server/app.py
@@ -13,18 +13,18 @@ CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/MS3_DB"
 mongo = PyMongo(app)
 
-@app.route('/', methods=['GET'])
-def get_data():
-    return jsonify({'message': 'Hello from Flask backend!'})
+# @app.route('/', methods=['GET'])
+# def get_data():
+#     return jsonify({'message': 'Hello from Flask backend!'})
 
-@app.route('/test_db', methods=['GET'])
-def test_db():
-    try:
+# @app.route('/test_db', methods=['GET'])
+# def test_db():
+#     try:
         
-        result = mongo.db.collection_name.find_one({'example_key': 'example_value'})
-        return jsonify({'message': f'Successfully connected to MongoDB. Result: {result}'}), 200
-    except Exception as e:
-        return jsonify({'message': f'Error connecting to MongoDB: {str(e)}'}), 500
+#         result = mongo.db.collection_name.find_one({'example_key': 'example_value'})
+#         return jsonify({'message': f'Successfully connected to MongoDB. Result: {result}'}), 200
+#     except Exception as e:
+#         return jsonify({'message': f'Error connecting to MongoDB: {str(e)}'}), 500
 
 
 if __name__ == '__main__':
