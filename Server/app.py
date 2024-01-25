b7dda0b236d40d9c87586edad1dca872d6ac659a
Esteban Sepulveda <tebansd@icloud.com>
2024-01-24 20:07:05 -0800

    changed DB to mongo

--- Server/app.py
+++ Server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from flask import Flask
+from flask_pymongo import PyMongo
 import os
 
 load_dotenv()
@@ -10,24 +10,22 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-db = SQLAlchemy(app)
-
-print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
+app.config["MONGO_URI"] = "mongodb://localhost:27017/MS3_DB"
+mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
 def get_data():
     return jsonify({'message': 'Hello from Flask backend!'})
 
-# TEST ROUTE TO SEE IF DATABASE CONNECTED
 @app.route('/test_db', methods=['GET'])
 def test_db():
     try:
-        result = db.session.execute(text("SELECT 1"))
-        return jsonify({'message': f'Successfully connected to the database. Result: {result.scalar()}'}), 200
+        
+        result = mongo.db.collection_name.find_one({'example_key': 'example_value'})
+        return jsonify({'message': f'Successfully connected to MongoDB. Result: {result}'}), 200
     except Exception as e:
-        return jsonify({'message': f'Error connecting to the database: {str(e)}'}), 500
+        return jsonify({'message': f'Error connecting to MongoDB: {str(e)}'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
