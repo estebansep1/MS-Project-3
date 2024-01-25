9008b6b73c2ff99c79fe045ff48a0cafd3edce85
Esteban Sepulveda <tebansd@icloud.com>
2024-01-24 18:09:14 -0800

    Connected the backend to database

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 from dotenv import load_dotenv
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 import os
 
 load_dotenv()
@@ -8,9 +10,24 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy(app)
+
+print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
+
 @app.route('/', methods=['GET'])
 def get_data():
     return jsonify({'message': 'Hello from Flask backend!'})
+
+# TEST ROUTE TO SEE IF DATABASE CONNECTED
+@app.route('/test_db', methods=['GET'])
+def test_db():
+    try:
+        result = db.session.execute(text("SELECT 1"))
+        return jsonify({'message': f'Successfully connected to the database. Result: {result.scalar()}'}), 200
+    except Exception as e:
+        return jsonify({'message': f'Error connecting to the database: {str(e)}'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
