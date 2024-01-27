7e97abc7b7dffff4984bc4c54ec1c4c7e427d446
Benjamin McConnaughy <bhmcconn@gmail.com>
2024-01-26 21:14:42 -0800

    converted server to python

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,45 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserModal from './Components/UserModal';
+import HomePage from './Components/HomePage';
+import LoginForm from './Components/LoginUser';
+import RegistrationForm from './Components/RegistrationForm';
+import './App.css';
+import './Components/todo.css';
 
 function App() {
-  const [data, setData] = useState('');
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [message, setMessage] = useState('Loading...');
+  const [secret, setSecret] = useState('Loading...');
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(process.env.BACKEND_URL);
-      setData(response.data.message);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+  useEffect(() => {
+    axios.get(`${API_URL}/`).then((resp) => {
+      setMessage(resp.data.message);
+      setSecret(resp.data.secret);
+    });
+  }, [API_URL]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
-    <div className="App">
-      <h1>Data from Flask Backend</h1>
-      <p>{data}</p>
+    <div>
+    <HomePage isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      <Router>
+        <Routes>
+          <Route path='/' element={<UserModal />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/register' element={<RegistrationForm />} />
+          <Route
+            path='/'
+            element={<HomePage isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
