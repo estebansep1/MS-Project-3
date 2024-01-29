45f4b886cb3abdefbd91cdfdb497f9e3f701ad57
Esteban Sepulveda <tebansd@icloud.com>
2024-01-28 19:52:24 -0800

    fixed connection between front and backend

---
+++ Client/src/Components/ChatRoom.jsx
@@ -0,0 +1,7 @@
+export default function ChatRoom() {
+    return (
+        <div>
+            <h1>ChatRoom</h1>
+        </div>
+    )
+}
---
+++ Client/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import ChatRoom from "./ChatRoom";
+
+export default function Home() {
+    return (
+        <div>
+            <ChatRoom />
+        </div>
+    )
+}
