98b355d7190e917b6548d21ec4925fc14beb8e56
Esteban Sepulveda <tebansd@icloud.com>
2024-01-29 11:23:01 -0800

    implemented ENV var

--- Server/index.js
+++ Server/index.js
@@ -1,39 +1,45 @@
-const express = require('exporess')
-const http = require('http')
-const Server = require("socket.io").Server
-const app = express()
-const path = require('path')
-
-const server = http.createServer(app)
-const io = http.createServer(server, {
-    cors:{
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { dirname, join } from 'path';
+import dotenv from 'dotenv';
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
         origin: "*"
     }
-})
+});
 
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname, "build")
+dotenv.config()
 
-app.use(express.static(buildPath))
+const _dirname = dirname("");
+const buildPath = join(_dirname, "build");
+
+app.use(express.static(buildPath));
 
 app.get("*", function(req, res) {
     res.sendFile(
-        path.join(_dirname, "../client/build/index.html"),
+        join(_dirname, "../client/build/index.html"),
         function (err) {
             if (err) {
-                res.status(500),send(err);
+                res.status(500).send(err);
             }
         }
-    )
-})
-
+    );
+});
 
 io.on("connection", (socket) => {
-    console.log('We are connected')
+    console.log('We are connected');
 
     socket.on("chat", chat => {
-        io.emit('chat', chat)
-    })
-})
+        io.emit('chat', chat);
+    });
 
-server.listen(5001, () => console.log('We are listening on port 5001'))
+    socket.on('disconnect', () => {
+        console.log('disconnected');
+    });
+});
+
+
+server.listen(process.env.PORT, () => console.log(`Listening on Port ${process.env.PORT}`));
