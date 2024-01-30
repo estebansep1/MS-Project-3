e367acf7d525e24ec145037042456d113a50f790
Sethgood7 <sethgooodman657@gmail.com>
2024-01-29 21:20:30 -0800

    need to pull

--- Client/src/Components/UserLogin.jsx
+++ Client/src/Components/UserLogin.jsx
@@ -6,16 +6,16 @@ import "../Scss/UserLogin.scss";
 export default function UserLogin({ setUser }) {
   const [user, setAUser] = useState("");
   const [pass, setAPass] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); 
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleSetUser() {
     if (!user || !pass) {
-      setErrorMessage("Username and password are required."); 
+      setErrorMessage("Username and password are required.");
       return;
     }
 
-    
-    const storedPassword = localStorage.getItem("password"); 
+
+    const storedPassword = localStorage.getItem("password");
 
     if (pass !== storedPassword) {
       setErrorMessage("Incorrect password.");
@@ -23,13 +23,14 @@ export default function UserLogin({ setUser }) {
     }
 
     localStorage.setItem("user", user);
-    
+    localStorage.setItem("pass", pass);
+
     setUser(user);
     localStorage.setItem(
       "avatar",
       `https://picsum.photos/id/${_.random(1, 1000)}/200/300`
     );
-    setErrorMessage(""); 
+    setErrorMessage("");
   }
 
   function handleKeyPress(keyEvent) {
