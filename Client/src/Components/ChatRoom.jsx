3056b7665ce0e2257d46ca105085d80b34bac01f
Sethgood7 <sethgooodman657@gmail.com>
2024-01-29 20:52:38 -0800

    pulled from main

--- Client/src/Components/ChatRoom.jsx
+++ Client/src/Components/ChatRoom.jsx
@@ -9,6 +9,7 @@ export default function ChatContainer() {
   let socketio = socketIOClient("http://localhost:5001");
   const [chats, setChats] = useState([]);
   const [user, setUser] = useState(localStorage.getItem("user"));
+  const [pass, setPass] = useState(localStorage.getItem("pass"));
   const avatar = localStorage.getItem("avatar");
   const messagesEndRef = useRef(null);
 
@@ -38,6 +39,7 @@ export default function ChatContainer() {
 
   function logout() {
     localStorage.removeItem("user");
+    localStorage.removeItem("pass");
     localStorage.removeItem("avatar");
     setUser("");
   }
