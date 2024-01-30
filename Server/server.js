31ff9b25640d2dfd380c668437dfe6e8b89d40dc
Esteban Sepulveda <tebansd@icloud.com>
2024-01-30 11:05:51 -0800

    updated code

--- Server/server.js
+++ Server/server.js
@@ -4,43 +4,37 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Enable CORS
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
 mongoose.connect('mongodb+srv://bhmcconn:12345@ms3db.r3lncfq.mongodb.net/yourDatabaseName', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Create a User model
 const User = mongoose.model('User', {
   username: String,
   password: String,
 });
 
-// Registration endpoint
 app.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    // Check if the username already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(400).json({ message: 'Username already exists' });
     }
 
-    // Create a new user
     const newUser = new User({
       username,
       password,
     });
 
-    // Save the user to the database
+  
     await newUser.save();
 
-    // Respond with success
+  
     res.status(201).json({ message: 'Registration successful' });
   } catch (error) {
     console.error(error);
