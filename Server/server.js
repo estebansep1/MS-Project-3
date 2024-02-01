09f07abfe95b3c258bbd001b31e320cbbe57526f
Sethgood7 <sethgooodman657@gmail.com>
2024-01-31 18:54:10 -0800

    created users route and controller

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const User = require('./models/user.js')
 
 app.use(cors());
 app.use(express.json());
@@ -12,42 +13,15 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 }
 
-
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const User = mongoose.model('User', {
-  username: String,
-  password: String,
-});
-
-app.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username already exists' });
-    }
-
-    const newUser = new User({
-      username,
-      password,
-    });
-
-  
-    await newUser.save();
-
-  
-    res.status(201).json({ message: 'Registration successful' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
