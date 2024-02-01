a4d6d697d5979d3671424e8df637deaca2849b27
Sethgood7 <sethgooodman657@gmail.com>
2024-01-31 20:57:46 -0800

    pushing to main

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,34 +1,35 @@
-const User = require('../models/user');
-
+const bcrypt = require('bcrypt');
+const User = require('../models/user'); 
 
 app.post('/register', async (req, res) => {
     try {
-      const { username, password } = req.body;
-  
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Username already exists' });
-      }
-  
-      const newUser = new User({
-        username,
-        password,
-      });
-  
-    
-      await newUser.save();
-  
-    
-      res.status(201).json({ message: 'Registration successful' });
+        const { username, password } = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        // Hash the password
+        const saltRounds = 10; // You can adjust the number of rounds
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Create a new user with the hashed password
+        const newUser = new User({
+            username,
+            password: hashedPassword, // Store the hashed password
+        });
+
+        // Save the new user
+        await newUser.save();
+
+        // Respond
+        res.status(201).json({ message: 'Registration successful' });
     } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
 
-
-//   const User = mongoose.model('User', {
-//     username: String,
-//     password: String,
-//   });
-
+module.exports = app;
--- Server/models/user.js
+++ Server/models/user.js
@@ -8,16 +8,14 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        unique: true,
+        required: true
     },
-    
 });
 
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
   
 
 
