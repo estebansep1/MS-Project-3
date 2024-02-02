bcfb27e0753706613311c6e77fffca83975cf42c
Sethgood7 <sethgooodman657@gmail.com>
2024-02-01 18:17:16 -0800

    need to push to main

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -2,29 +2,30 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user'); 
 
 app.post('/register', async (req, res) => {
+    console.log('test')
     try {
         const { username, password } = req.body;
+        console.log(username)
 
-        // Check if user already exists
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(400).json({ message: 'Username already exists' });
         }
 
-        // Hash the password
-        const saltRounds = 10; // You can adjust the number of rounds
+
+        const saltRounds = 10; 
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        // Create a new user with the hashed password
+
         const newUser = new User({
             username,
-            password: hashedPassword, // Store the hashed password
+            password: hashedPassword,
         });
 
-        // Save the new user
+
         await newUser.save();
 
-        // Respond
+
         res.status(201).json({ message: 'Registration successful' });
     } catch (error) {
         console.error(error);
